4d469709b743bdb7007fe6dcf1ce046df0d5ffc4
rohitsar <rohitsar@umich.edu>
2022-12-26 17:41:55 -0500

    login page almost complete

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,3 +1,4 @@
+import { buildQueries } from "@testing-library/react";
 import { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
@@ -21,12 +22,50 @@ export default function Login(){
 
     return (
         <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-            <div className="flex w-2/5">
-                <img src="/images/wolverine_in_convertible.png" style="width:30"></img>
-            </div>
-            <div className="flex flex-col w-2/5">
-                <p>login</p>
-            </div>
+        <div className="flex w-3/5">
+          <img src="/images/wolverine_in_convertible.png" alt="Wolverine logo" />
         </div>
-    );
+        <div className="flex flex-col w-2/5">
+          <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+            <h1 className="flex justify-center w-full">
+              UMRideHub
+            </h1>
+  
+            {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+  
+            <form onSubmit={handleLogin} method="POST">
+              <input
+                aria-label="Enter your email address"
+                type="text"
+                placeholder="Email address"
+                className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                onChange={({ target }) => setEmailAddress(target.value)}
+                value={emailAddress}
+              />
+              <input
+                aria-label="Enter your password"
+                type="password"
+                placeholder="Password"
+                className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                onChange={({ target }) => setPassword(target.value)}
+                value={password}
+              />
+              <button
+                disabled={isInvalid}
+                type="submit"
+                className={`bg-blue-medium text-white w-full rounded h-8 font-bold
+              ${isInvalid && 'opacity-50'}`}
+              >
+                Login
+              </button>
+            </form>
+          </div>
+          <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
+            <p className="text-sm">
+              Don't have an account?{` `}
+            </p>
+          </div>
+        </div>
+      </div>
+    )
 }
