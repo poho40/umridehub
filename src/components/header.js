f27c2155065fb0d3bd569fed871d2fcbd3b8e4c9
rohitsar <rohitsar@umich.edu>
2022-12-27 18:00:00 -0500

    header file

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
 import UserContext from "../context/user";
 import * as ROUTES from '../constants/routes'
+import Dashboard from "../pages/dashboard";
 
 
 
@@ -19,34 +20,62 @@ export default function Header() {
                 <div className="text-gray-700 text-center flex items-center align-items cursor-pointer">
                     <h1 className="flex justify-center-w-full">
                         <Link to={ROUTES.DASHBOARD}>
-                        <img src="/images/typography.png" alt="Typography Logo" className="mt-2 w-6/12"/>
+                            <img src="/images/typography.png" alt="Typography Logo" className="mt-2 w-6/12"/>
                         </Link>
                     </h1>
                 </div>
                 <div className="text-gray-700 text-center flex items-center align-items">
-                    {user ? (
-                        <>
-                            <Link to={ROUTES.DASHBOARD}
-                            aria-label = "Dashboard">
-                                <p>home button</p>
-                            </Link>
-                            
-                            <button 
-                                type="button"
-                                title="Sign Out"
-                                onClick={()=> firebase.auth().signOut()}
-                                onKeyDown = {(event)=>{
-                                    if(event.key==='Enter'){
-                                        firebase.auth().signOut();
-                                    }
-                                }}
-                            />
-                        </>
-                    ) :(
-                        <>
-                        </>
-                    )
+                {!user ? (     
+                    <>
+                <Link to={ROUTES.DASHBOARD} aria-label="Dashboard">
+                  <svg
+                    className="w-8 mr-6 text-black-light cursor-pointer"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                    />
+                  </svg>
+                </Link>
+
+                <button
+                  type="button"
+                  title="Sign Out"
+                  onClick={() => {
+                    firebase.auth().signOut();
+                  }}
+                  onKeyDown={(event) => {
+                    if (event.key === 'Enter') {
+                      firebase.auth().signOut();
                     }
+                  }}
+                >
+                  <svg
+                    className="w-8 mr-6 text-black-light cursor-pointer"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                    />
+                  </svg>
+                </button>
+                </> 
+                ) : (
+                    <>
+                    </>
+                )}
                 </div>
             </div>
          </div>
