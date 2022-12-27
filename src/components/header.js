1715242d4e177045759049378c3498fd5bff6d19
Ritij Jutur <rjutur@umich.edu>
2022-12-27 17:10:18 -0500

    push?

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,42 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
+import UserContext from "../context/user";
+import * as ROUTES from '../constants/routes'
+
+
+
 export default function Header() {
-    return <p> I am the Header</p>
+    const {firebase} = useContext(FirebaseContext);
+    const {user} = useContext(UserContext);
+
+
+    return(
+    <header className="h-16 border-b border-gray-primary mb-8" style={{
+    }}>
+         <div className="container mx-auto max-w-screen-lg h-full">
+            <div className="justify-between flex h-full">
+                <div className="text-gray-700 text-center flex items-center align-items cursor-pointer">
+                    <h1 className="flex justify-center-w-full">
+                        <Link to={ROUTES.DASHBOARD}>
+                        <img src="/images/typography.png" alt="Typography Logo" className="mt-2 w-6/12"/>
+                        </Link>
+                    </h1>
+                </div>
+                <div className="text-gray-700 text-center flex items-center align-items">
+                    {user ? (
+                        <>
+                            <Link to={ROUTES.DASHBOARD} arial-label = "Dashboard">
+                            </Link>
+                        </>
+                    ) :(
+                        <>
+                        </>
+                    )
+                    }
+                </div>
+            </div>
+         </div>
+    </header>
+    )
 }
