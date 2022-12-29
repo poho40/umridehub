42e80f067a51ee46ad3538ed2f88bd731dec21a9
rohitsar <rohitsar@umich.edu>
2022-12-28 21:26:40 -0500

    getUserByUserId function

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -10,3 +10,14 @@ export async function doesEmailExist(emailAddress){
         return result.docs.map((user) => user.data().length > 0);
 
 }
+
+export async function getUserByUserId(userId){
+    const result = await firebase.firestore().collection('users').where('userId', '==', userId).get();
+
+    const user = result.docs.map((item) => ({
+         ...item.data(),
+        docId: item.id
+    }));
+
+    return user;
+}
