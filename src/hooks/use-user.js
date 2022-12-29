d7e05706729f732944283dbe2e0d905a9c670ad7
Ritij Jutur <rjutur@umich.edu>
2022-12-28 21:58:11 -0500

    syntax

--- src/hooks/use-user.js
+++ src/hooks/use-user.js
@@ -8,7 +8,7 @@ export default function useUser() {
 
     useEffect(() => {
         async function getUserObjByUserId() {
-            const response  = await getUserByUserId(user.uid);
+            const [response]  = await getUserByUserId(user.uid);
             setActiveUser(response);  
         }
         if(user?.uid) {
