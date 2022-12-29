e6b402199af419d26a21f9cadfaec3bb0fd4d869
rohitsar <rohitsar@umich.edu>
2022-12-29 18:36:15 -0500

    recent.js

--- src/components/recent.js
+++ src/components/recent.js
@@ -6,7 +6,16 @@ export default function Recent() {
     const {posts} = usePosts(); 
 
 
-    return <div className="container col-span-3">
-        <p>I am the recents</p>
+    return (<div className="container col-span-">
+        {posts ? (
+            <>
+            {[...new Array(4)].map((_, index) =>
+                <Skeleton key = {index} count = {1} width = {320} height = {400} className = "mb-3"/>
+            )}
+            </>
+        ) : (
+            posts.map((content) => <p>{content.caption}</p>)
+        )}
     </div>
+    );
 }
