41b7093a383acf55b9380f5585287b0e7b99034d
rohitsar <rohitsar@umich.edu>
2022-12-30 17:25:08 -0500

    protected routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import * as ROUTES from './constants/routes';
 import UserContext from "./context/user";
 import useAuthListener from "./hooks/use-auth-listener";
+import ProtectedRoute from "./helpers/protected.route";
 
 const Login = lazy(()=>import('./pages/login'));
 const SignUp = lazy(()=>import('./pages/signup'));
@@ -21,9 +22,9 @@ export default function App() {
           <Routes>
             <Route path = {ROUTES.LOGIN} element={<Login/>}/>
             <Route path = {ROUTES.SIGNUP} element={<SignUp/>}/>
-            <Route path = {ROUTES.DASHBOARD} element={<Dashboard/>}/>
-            <Route path = {ROUTES.PROFILE} element={<Profile/>}/>
-            <Route path = {ROUTES.CREATEPOST} element={<CreatePost/>}/>
+            <Route path = {ROUTES.DASHBOARD} element={<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
+            <Route path = {ROUTES.PROFILE} element={<ProtectedRoute><Profile/></ProtectedRoute>}/>
+            <Route path = {ROUTES.CREATEPOST} element={<ProtectedRoute><CreatePost/></ProtectedRoute>}/>
             <Route element = {<NotFound/>}/>
           </Routes>
         </Suspense>
