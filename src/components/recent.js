d8ee15f5cf221cfb5edf20cbba8a650dbf2088d0
rohitsar <rohitsar@umich.edu>
2022-12-29 19:01:58 -0500

    recent

--- src/components/recent.js
+++ src/components/recent.js
@@ -6,15 +6,27 @@ export default function Recent() {
     const {posts} = usePosts(); 
 
 
-    return (<div className="container col-span-">
+    return (<div className="container col-span-3">
         {!posts ? (
             <>
             {[...new Array(4)].map((_, index) =>
-                <Skeleton key = {index} count = {1} width = {320} height = {400} className = "mb-3"/>
+                <Skeleton key = {index} count = {4} width = {320} height = {400} className = "mb-3"/>
             )}
             </>
         ) : (
-            posts.map((content) => <p>{content.title}</p>)
+            posts.map((content) => <div class="max-w-sm rounded overflow-hidden shadow-lg">
+            <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <p class="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+              </p>
+            </div>
+            <div class="px-6 pt-4 pb-2">
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+            </div>
+          </div>)
         )}
     </div>
     );
