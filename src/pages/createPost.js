eb1755b65802931eb916744418002cc9c294725b
Ritij Jutur <rjutur@umich.edu>
2023-01-01 19:45:35 -0500

    create post

--- src/pages/createPost.js
+++ src/pages/createPost.js
@@ -4,6 +4,8 @@ import FirebaseContext from "../context/firebase";
 import UserContext from "../context/user";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import * as ROUTES from '../constants/routes';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpFromBracket } from "@fortawesome/free-solid-svg-icons";
 
 
     export default function AddPost() {
@@ -38,40 +40,27 @@ import * as ROUTES from '../constants/routes';
     return(
         <div className="bg-gray-background">
             <Header />
-            <form className="w-full max-w-lg justify-center items-center" style={{width:"80%", alignContent:"center"}} method="POST"
+            <form className="w-full max-w-lg justify-center grid place-items-center" style={{alignContent:"center", alignItems:'center', paddingBottom:"100px", paddingTop:"15px"}} method="POST"
             onSubmit={(event) => handlePost(event)
         }>
             <div className="flex flex-wrap -mx-3 mb-6">
     <div className="w-full px-3">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-        Title
-      </label>
-      <input maxLength = "50em" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="title" type="text" placeholder="Title" onChange={({ target }) => setTitle(target.value)}/>
+      <input maxLength = "50em" className="inputCSS" id="title" type="text" placeholder="Title" onChange={({ target }) => setTitle(target.value)}/>
       <p className="text-gray-600 text-xs italic"></p>
     </div>
   </div>
   <div className="flex flex-wrap -mx-3 mb-3">
     <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0 mr-3">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-            Departure Location
-      </label>
-      <input onChange={({ target }) => setDepartureLocation(target.value)}className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane"/>
-      <p className="text-red-500 text-xs italic">Please fill out this field.</p>
+      <input onChange={({ target }) => setDepartureLocation(target.value)}className="inputCSS" id="grid-first-name" type="text" placeholder="Departure Location"/>
     </div>
     <div className="w-full md:w-1/2 px-3">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-        Destination 
-      </label>
-      <input onChange={({ target }) => setDestination(target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe"/>
+      <input onChange={({ target }) => setDestination(target.value)} className="inputCSS" id="grid-last-name" type="text" placeholder="Destination"/>
     </div>
   </div>
   <div className="flex flex-wrap -mx-3 mb-6">
     <div className="w-full md:w-1/2 px-3">
-        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
-          Mode of Transportation
-        </label>
-        <select onChange={({ target }) => setTransportation(target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" name="cars">
-          <option disabled selected value> -- select an option -- </option>
+        <select onChange={({ target }) => setTransportation(target.value)} className="inputCSS" id="grid-last-name" type="text" name="cars">
+          <option disabled selected value> -- Mode of Transportation -- </option>
         <option value="Lyft">Lyft</option>
           <option value="Uber">Uber</option>
           <option value="Carpool">Carpool</option>
@@ -82,25 +71,45 @@ import * as ROUTES from '../constants/routes';
     </div>
   </div>
   <div className="flex flex-wrap -mx-3 mb-6">
-  <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+  <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0 mr-3">
       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
             Departure Date
       </label>
-      <input onChange={({ target }) => setDepartureDate(target.value)} min = {new Date().toISOString().split("T")[0]} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="date" type="date"/>
+      <input onChange={({ target }) => setDepartureDate(target.value)} min = {new Date().toISOString().split("T")[0]} className="inputCSS" id="date" type="date"/>
     </div>
     <div className="w-full md:w-1/2 px-3">
       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
         Departure Time
       </label>
-      <input onChange={({ target }) => setDepartureTime(target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="time" type="time"/>
+      <input onChange={({ target }) => setDepartureTime(target.value)} className="inputCSS" id="time" type="time"/>
     </div>
   </div>
     <button
-      className={`text-sm font-bold text-blue-medium ${(!title || !departureTime ||!departureDate || !departureLocation || !destinationLocation || !transportation) && 'opacity-25'}`}
+    style={{  alignItems: "center",
+      backgroundColor: "#FFCB05",
+      borderRadius: "24px",
+      boxSizing: "border-box",
+      boxShadow:"2px 2px 2px 2px #00274C",
+      color: "#3c4043",
+      cursor: "pointer",
+      display: "inline-flex",
+      fontSize: "14px",
+      fontWeight: "500",
+      height: "40px",
+      justifyContent: "center",
+      letterSpacing: ".25px",
+      lineHeight: "normal",
+      maxWidth: "100%",
+      overflow: "visible",
+      position: "relative",
+      textAlign: "center",
+      width: "30%",
+      }}
+      className={`text-sm font-bold grid place-items-center${(!title || !departureTime ||!departureDate || !departureLocation || !destinationLocation || !transportation) && 'opacity-25'}`}
       type="button"
       onClick={handlePost}
         disabled = {(!title || !departureTime ||!departureDate || !departureLocation || !destinationLocation || !transportation)}>
-            Create Post
+            <FontAwesomeIcon className="mr-1" icon={faArrowUpFromBracket}></FontAwesomeIcon> <p> Create Post</p>
     </button>
 </form>
  </div>
